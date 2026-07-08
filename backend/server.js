eab6adf51a9a54ca240d4e3cb8305e1335052934
Yashwanth <yashwanth.gr@example.com>
2026-07-08 12:57:54 +0530

    fix: remove duplicate path declaration in server.js

--- backend/server.js
+++ backend/server.js
@@ -4233,7 +4233,6 @@ module.exports = {
   resolvePackageForPrompt
 };
 
-const path = require("path");
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
