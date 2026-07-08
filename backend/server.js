1b77b465ce5b137dd71136fe6428444e57b8ee12
Yashwanth <yashwanth.gr@example.com>
2026-07-08 13:07:42 +0530

    fix: use JS RegExp literal for Express v5 catch-all wildcard

--- backend/server.js
+++ backend/server.js
@@ -4234,7 +4234,7 @@ module.exports = {
 };
 
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
-app.get("(.*)", (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
 
